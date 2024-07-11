a4550cfe3cc886fe9867a24593095feab7639db5
sharkz <mosenjemudogoz@gmail.com>
2024-07-11 15:57:13 +0300

    layouts

--- lib/main.dart
+++ lib/main.dart
@@ -10,29 +10,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Hello'),
+      home: const Main(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class Main extends StatelessWidget {
+  const Main({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: const Text("title"),
       ),
-      body: const Center(
-        child: Text("Hello Sharkz"),
+      body: Container(
+        height: 150,
+        child: const Card(
+          margin: EdgeInsets.all(15.0),
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Image(
+                  image: AssetImage("assets/images/user.png"),
+                  height: 130.0,
+                ),
+                Column(
+                  children: [Text("name"), Text("description"), Text("price")],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
