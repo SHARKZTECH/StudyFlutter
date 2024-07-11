598f5fe89aa0b559148c5b5b57e2397b2e0afb02
sharkz <mosenjemudogoz@gmail.com>
2024-07-11 17:39:34 +0300

    state mgtmt rating

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,101 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Gesture(),
+      home: const RatingBox(),
+    );
+  }
+}
+
+class RatingBox extends StatefulWidget {
+  const RatingBox({super.key});
+
+  @override
+  _RatingBox createState() => _RatingBox();
+}
+
+class _RatingBox extends State<RatingBox> {
+  int _rating = 0;
+
+  void _setRatingAsOne() {
+    setState(() {
+      _rating = 1;
+    });
+  }
+
+  void _setRatingAsTwo() {
+    setState(() {
+      _rating = 2;
+    });
+  }
+
+  void _setRatingAsThree() {
+    setState(() {
+      _rating = 3;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double size = 20;
+    print(_rating);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 1
+                ? Icon(
+                    Icons.star,
+                    size: size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: size,
+                  )),
+            color: Colors.red[500],
+            iconSize: size,
+            onPressed: _setRatingAsOne,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 2
+                ? Icon(
+                    Icons.star,
+                    size: size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: size,
+                  )),
+            color: Colors.red[500],
+            iconSize: size,
+            onPressed: _setRatingAsTwo,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 3
+                ? Icon(
+                    Icons.star,
+                    size: size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: size,
+                  )),
+            color: Colors.red[500],
+            iconSize: size,
+            onPressed: _setRatingAsThree,
+          ),
+        )
+      ],
     );
   }
 }
